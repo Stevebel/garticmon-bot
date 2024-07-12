a803f9171d90a4faa9d9eb69d6760d02f49c3c59
Steven B <373152+Stevebel@users.noreply.github.com>
2024-07-12 18:19:38 -0400

    Add icon support

--- index_icon.py
+++ index_icon.py
@@ -1,98 +1,44 @@
-import sys
 from PIL import Image
-from os import path
-import math
 
-def process_image(filename, outname, targetPal):
+from index_spritesheet import get_palette, rgb_tuple_to_hex
+
+
+def test_icon(filename, targetPal):
     with Image.open(filename) as img:
         with Image.open(targetPal) as palImg:
-            c = img.convert(mode='RGB').quantize(colors=16, dither=Image.NONE)
-            bottomRight = c.getpixel((c.width - 1, c.height - 1))
-            if (bottomRight != 0):
-                swapPalette(c, bottomRight, 0)
-            changePaletteZero(c, palImg.getpalette()[0:3])
-            matchPalette(c, palImg)
-            c.info['transparency'] = None
-            clearPaletteAfter16(c)
-            c.save(outname)
-            print('Saved output to ', outname)
-            return outname
+            icon_palette = get_palette(img)
+            target_palette = get_palette(palImg)
+            issues = detect_palette_issues(icon_palette, target_palette)
+            return issues
 
-def swapPalette(img, fromIdx, toIdx):
-    pal = img.getpalette()
-    valToMove = pal[toIdx * 3: (toIdx + 1) * 3]
-    for i in range(3):
-        pal[(toIdx * 3) + i] = pal[(fromIdx * 3) + i]
-        pal[(fromIdx * 3) + i] = valToMove[i]
-    img.putpalette(pal)
-    swapColor(img, fromIdx, toIdx)
 
-def swapColor(img, fromVal, toVal):
-    data = list(img.getdata())
-    for i in range(len(data)):
-        if (data[i] == fromVal):
-            data[i] = toVal
-        elif(data[i] == toVal):
-            data[i] = fromVal
-    img.putdata(data)
-
-def changePaletteZero(img, color = [255, 0, 255]):
-    pal = img.getpalette()
-    pal[0:3] = color
-    img.putpalette(pal)
-
-def clearPaletteAfter16(img):
-    pal = img.getpalette()
-    for i in range(48, len(pal)):
-        pal[i] = 0
-    img.putpalette(pal)
-
-def matchPalette(img, palImg):
-    targetPal = paletteToTuples(palImg.getpalette()[0:48])
-    currPal = paletteToTuples(img.getpalette()[0:len(img.getcolors()) * 3])
-    targetIdx = [closestIndex(currPal[i], targetPal) for i in range(len(currPal))]
-    data = list(img.getdata())
-    for i in range(len(data)):
-        if (data[i] < len(targetIdx)):
-            data[i] = targetIdx[data[i]]
-        else:
-            data[i] = 0
-    img.putdata(data)
-    img.putpalette(palImg.getpalette())
-
-def paletteToTuples(pal):
-    return list(zip(*[pal[i::3] for i in range(3)]))
-
-def closestIndex(c, pal):
-    # If the color is in the palette, return its index
-    if (c in pal):
-        return pal.index(c)
-    # Otherwise, find the closest color in the palette
-    min_d = float('inf')
-    best = 0
-    r1, g1, b1 = c
-    for i, (r2, g2, b2) in enumerate(pal[1:]):
-        # Color diff from https://stackoverflow.com/questions/1847092/given-an-rgb-value-what-would-be-the-best-way-to-find-the-closest-match-in-the-d
-        d = ((r2-r1)*0.30)**2 + ((g2-g1)*0.59)**2 + ((b2-b1)*0.11)**2
-        if d < min_d:
-            min_d = d
-            best = i
-    return best + 1
-
-def getDistance(c1, c2):
-    # Get simple Euclidean distance in RGB space.
-    # This is a very poor approximation but works fine as long as
-    # the colors are very close to the palette
-    r = abs(c2[0] - c1[0])
-    g = abs(c2[1] - c1[1])
-    b = abs(c2[2] - c2[2])
-    return math.sqrt(r*r + g*g + b*b)
-
-if __name__ == '__main__':
-    palNum = sys.argv[1]
-    filename = sys.argv[2]
-    outname = filename.replace('.png', '') + '_fixed.png'
-    if (len(sys.argv) > 3):
-        outname = sys.argv[3]
-    print('Indexing ' + filename + ' to palette #' + palNum)
-    process_image(filename, outname, path.join('assets', 'palettes', 'IconPalette' + palNum + '.png'))
+# Detect colors in icon palette that don't exist in target palette
+def detect_palette_issues(icon_palette, target_palette, icon_img: Image):
+    issues = []
+    if len(icon_palette) > 16:
+        issues.append(
+            "Icon palette has more than 16 colors (%d colors()" % len(icon_palette)
+        )
+        return issues
+    icon_palette_set = set(icon_palette)
+    target_palette_set = set(target_palette)
+    extra_colors = icon_palette_set - target_palette_set
+    if len(extra_colors) > 0:
+        color_counts = {}
+        for i in range(len(icon_img)):
+            color = icon_img[i]
+            if color in extra_colors:
+                color_counts[color] = color_counts.get(color, 0) + 1
+        # Format color counts as a list of hex values with pixel counts (ex. "#ff0000 (2 pixels), #00ff00 (1 pixel)") in order of count descending
+        ordered_color_counts = sorted(
+            color_counts.items(), key=lambda x: x[1], reverse=True
+        )
+        formatted_counts = [
+            rgb_tuple_to_hex(color) + " (" + str(count) + " pixels)"
+            for color, count in ordered_color_counts
+        ]
+        issues.append(
+            "Extra colors in icon palette compared to the battle sprite: %s"
+            % ", ".join(formatted_counts)
+        )
+    return issues
--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import datetime
 import os
 from random import randint
 from dotenv import load_dotenv
+import pytz
+from index_icon import test_icon
 from unidecode import unidecode
 import discord
 from PIL import Image
 import index_spritesheet
 
+DEBUG_MODE = False
+
 BOT_CHANNEL_NAME = "bot-stuff"
 MON_SPRITE_CHANNEL_NAME = "garticmon"
 MON_SPRITE_FOLDER = "garticmon"
 PROCESSED_SPRITE_FOLDER = "processed"
-MAX_MESSAGES = 10
+MAX_MESSAGES = 1000
 
 BATTLE_SPRITE_WIDTH = 256
 BOX_ICON_WIDTH = 32
@@ -20,11 +24,26 @@ MON_SPRITE_SUCCESS_MESSAGES = [
     "Good job {user}, the sprites for {mon} look solid!",
     "Wow you got the sprites right for {mon} on your first try, {user}!",
     "Everyone give {user} some dancing Binglys for their perfect {mon} sprites!",
+    "You're on fire, {user}! Those {mon} sprites are flawless!",
+    "Boom! {user} did it again, the sprites for {mon} are perfect!",
+    "The sprites for {mon} are ready to be dropped into the game, {user}!",
+    "{mon}'s sprites are good to go, bet you weren't expecting that {user}!",
+    "Hmm, not sure about the sprites for {mon}... they might be my favorite yet, {user}! Keep it up!",
+    "Blessu has blessed you with those flawless {mon} sprites, {user}!",
+    "...\n\n...\n\n(Hector approves of your sprites for {mon}, {user})",
 ]
 MON_SPRITE_ISSUE_MESSAGES = [
     "Hmm, not so sure about the sprites for {mon}, take a look at these issues {user}:",
     "Those {mon} pixels look pretty sharp but I have a few pointers for you, {user}:",
     "They can't all be winners, {user}, but {mon} can get back in the game after you fix this:",
+    "You might have seen this coming, {user}, I found some issues with the sprites for {mon}:",
+    "Oops, think you misplaced a pixel, {user}. Here's what I found with the sprites for {mon}:",
+    "Hey {user}, how's your day going? Great. Anyway, unrelated, I found some issues with the sprites for {mon}:",
+    "Great work {user}! The sprites for {mon} look perfect... except for these issues:",
+    "Wow, so many pixels in the sprites for {mon}, no wonder you missed these issues {user}:",
+    "You know the problem with the GBA? It's dumb, {user}, it can't handle the sprites for {mon} unless you fix these issues:",
+    "{user} your horoscope for the {mon} sprites is in, here's what it says:",
+    "Life would be boring without any challenges, {user}. On that note, I found some issues with the sprites for {mon}:",
 ]
 
 intents = discord.Intents.default()
@@ -45,7 +64,8 @@ async def on_ready():
         bot_channel = get_bot_channel(server)
         if bot_channel is None:
             continue
-        last_post_time = None #await get_last_post_time(bot_channel)
+        utc = pytz.timezone('UTC')
+        last_post_time = utc.localize(datetime.datetime.now()) - datetime.timedelta(days=100) #await get_last_post_time(bot_channel)
         print(last_post_time)
         await check_mon_sprites(server, mon_sprite_channel, last_post_time)
 
@@ -81,28 +101,33 @@ async def check_mon_sprites(server: discord.Guild, channel: discord.TextChannel,
             continue
         if message.author.bot:
             continue
-        print(message.content)
         # Download any images in the message
         mon_name = message.content.split(" ")[0] or "default"
         folder_name = unidecode(mon_name).lower().replace(" ", "-")
         base_path = os.path.join(MON_SPRITE_FOLDER, folder_name)
         os.makedirs(base_path, exist_ok=True)
-        print("Saving images to", base_path)
         for attachment in message.attachments:
+            filenames = []
             if attachment.content_type.startswith("image/"):
                 filename = os.path.join(base_path, attachment.filename)
                 await attachment.save(filename)
+                filenames.append(filename)
+            battle_filename = None
+            icon_filename = None
+            for filename in filenames:
                 with Image.open(filename) as img:
                     if(img.width == BATTLE_SPRITE_WIDTH):
-                        success = await test_battle_sprite(server, filename, mon_name, message)
-                        if success:
-                            await message.remove_reaction(rejected_emoji, client.user)
-                        else:
-                            await message.add_reaction(rejected_emoji)
+                        battle_filename = filename
                     elif(img.width == BOX_ICON_WIDTH):
-                        await test_box_icon(server, filename, base_path)
+                        icon_filename = filename
+            if battle_filename is not None:
+                success = await test_battle_sprite(server, battle_filename, icon_filename, mon_name, message)
+                if success:
+                    await message.remove_reaction(rejected_emoji, client.user)
+                else:
+                    await message.add_reaction(rejected_emoji)
 
-async def test_battle_sprite(server: discord.Guild, filename: str, mon: str, original_message: discord.Message):
+async def test_battle_sprite(server: discord.Guild, filename: str, icon_filename: str, mon: str, original_message: discord.Message):
     print("Testing battle sprite", filename)
     folder_name = unidecode(mon).lower().replace(" ", "-")
     folder_name = os.path.join(PROCESSED_SPRITE_FOLDER, folder_name, "")
@@ -111,22 +136,32 @@ async def test_battle_sprite(server: discord.Guild, filename: str, mon: str, ori
     user_tag = original_message.author.mention
     msg_content = ""
     msg_files = []
-    if result["success"]:
-        msg_content = MON_SPRITE_SUCCESS_MESSAGES[randint(0, len(MON_SPRITE_SUCCESS_MESSAGES)-1)].format(user=user_tag, mon=mon)
-        return True
-    else:
-        msg_content = MON_SPRITE_ISSUE_MESSAGES[randint(0, len(MON_SPRITE_ISSUE_MESSAGES)-1)].format(user=user_tag, mon=mon)
-        msg_content += "\n\n" + "\n\n".join(result["issues"])
-        if result["diff_filename"] is not None:
-            msg_files.append(discord.File(result["diff_filename"]))
-    # print(msg_content)
     bot_channel = get_bot_channel(server)
-    await bot_channel.send(msg_content, files=msg_files)
+    if result["success"]:
+        issues = icon_filename and test_icon(icon_filename, result["front_file_name"]) or []
+        if len(issues) > 0:
+            await send_sprite_issue_message(bot_channel, issues, mon, user_tag)
+            return False
+        msg_content = MON_SPRITE_SUCCESS_MESSAGES[randint(0, len(MON_SPRITE_SUCCESS_MESSAGES)-1)].format(user=user_tag, mon=mon)
+        # await send_message(bot_channel, msg_content)
+        return True
+    
+    if result["diff_filename"]:
+        msg_files.append(discord.File(result["diff_filename"]))
+    await send_sprite_issue_message(bot_channel, result["issues"], mon, user_tag, files=msg_files)
     return False
-        
 
-async def test_box_icon(server: discord.Guild, filename: str, folder_name: str):
-    print("Testing box icon", filename)
+async def send_sprite_issue_message(channel: discord.TextChannel, issues: list[str], mon: str, user_tag: str, files: list[discord.File] = None):
+    msg_content = MON_SPRITE_ISSUE_MESSAGES[randint(0, len(MON_SPRITE_ISSUE_MESSAGES)-1)].format(user=user_tag, mon=mon)
+    msg_content += "\n\n> " + "\n\n> ".join(issues)
+    await send_message(channel, msg_content, files)
+
+        
+async def send_message(channel: discord.TextChannel, content: str, files: list[discord.File] = None):
+    if DEBUG_MODE:
+        print("[DEBUG] Not sending message:", content)
+    else:
+        await channel.send(content, files=files)
 
 try:
     load_dotenv()
